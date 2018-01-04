d2420eeebb0a68332a65a82edbb818aa50b241b8
Supakit Chaiman <bekaza759@gmail.com>
2018-01-04 20:17:40 +0700

    Edit route and Make api doc

--- client/src/services/UsersServices.js
+++ client/src/services/UsersServices.js
@@ -2,19 +2,19 @@ import Api from '@/services/Api'
 
 export default {
 	showAll () {
-		return Api().get(`users/get`)
+		return Api().get(`user/get`)
 	},
 	showById (userId) {
-		return Api().get(`users/${userId}`)
+		return Api().get(`user/${userId}`)
 	},
 	searchByFirstname (firstname) {
-		return Api().get(`users/firstname/${firstname}`)
+		return Api().get(`user/firstname/${firstname}`)
 	},
 	searchByLastname (lastname) {
-		return Api().get(`users/lastname/${lastname}`)
+		return Api().get(`user/lastname/${lastname}`)
 	},
 	searchByEmail (email) {
-		return Api().get(`users/email/${email}`)
+		return Api().get(`user/email/${email}`)
 	},
 	searchByGender (gender) {
 		return Api().get(`users/gender/${gender}`)
--- server/src/routes.js
+++ server/src/routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
         res.contentType('application/json')
         next()
     })
-    app.get('/users/get', UsersController.index)
+    app.get('/user/get', UsersController.index)
     /**
     * @api {get} /user/:id Get User By id.
     * @apiVersion 0.0.1
@@ -17,7 +17,7 @@ module.exports = (app) => {
     * @apiError UserNotFound The id of the User was not found.
     * @apiUse UserNotFoundError
     * */
-    app.get('/users/:userId(\\d+)/', UsersController.getById)
+    app.get('/user/:userId(\\d+)/', UsersController.getById)
     /**
     * @api {get} /user/firstname/:fname Get User By firstname.
     * @apiVersion 0.0.1
@@ -29,7 +29,7 @@ module.exports = (app) => {
     * @apiError UserNotFound The firstname of the User was not found.
     * @apiUse UserNotFoundError
     * */
-    app.get('/users/firstname/:fname', UsersController.getByFirstName)
+    app.get('/user/firstname/:fname', UsersController.getByFirstName)
     /**
     * @api {get} /user/lastname/:lname Get User By lastname.
     * @apiVersion 0.0.1
@@ -41,7 +41,7 @@ module.exports = (app) => {
     * @apiError UserNotFound The lastname of the User was not found.
     * @apiUse UserNotFoundError
     * */
-    app.get('/users/lastname/:lname', UsersController.getByLastName)
+    app.get('/user/lastname/:lname', UsersController.getByLastName)
     /**
     * @api {get} /user/email/:email Get User By email.
     * @apiVersion 0.0.1
@@ -53,9 +53,9 @@ module.exports = (app) => {
     * @apiError UserNotFound The email of the User was not found.
     * @apiUse UserNotFoundError
     * */
-    app.get('/users/email/:email', UsersController.getByEmail)
+    app.get('/user/email/:email', UsersController.getByEmail)
     /**
-    * @api {get} /user/gender/:gender Get User By gender.
+    * @api {get} /users/gender/:gender Get User By gender.
     * @apiVersion 0.0.1
     * @apiName GetUserByGender
     * @apiGroup User
@@ -67,7 +67,7 @@ module.exports = (app) => {
     * */
     app.get('/users/gender/:gender', UsersController.getByGender)
     /**
-    * @api {get} /user/age/:age Get User By age.
+    * @api {get} /users/age/:age Get User By age.
     * @apiVersion 0.0.1
     * @apiName GetUserByAge
     * @apiGroup User
@@ -79,7 +79,7 @@ module.exports = (app) => {
     * */
     app.get('/users/age/:age(\\d+)', UsersController.getByAge)
     /**
-    * @api {get} /user/age/:min-:max Get User By between age.
+    * @api {get} /users/age/:min-:max Get User By between age.
     * @apiVersion 0.0.1
     * @apiName GetUserByBetweenAge
     * @apiGroup User
@@ -91,20 +91,4 @@ module.exports = (app) => {
     * @apiUse UserNotFoundError
     * */
     app.get('/users/age/:min-:max', UsersController.getByBetweenAge)
-    /**
-    * @api {get} /users/search?firstname=&lastname=&email=&age=&gender= Get User By Query String.
-    * @apiVersion 0.0.1
-    * @apiName GetUserByQueryString
-    * @apiGroup User
-    *
-    * @apiParam {String} firstname Users first_name.
-    * @apiParam {String} lastname Users last_name.
-    * @apiParam {String} email Users email.
-    * @apiParam {String="male", "female"} gender Users gender.
-    * @apiParam {Number} age Users age.
-    * @apiUse UserSuccessReturnArray
-    * @apiError UserNotFound The age of the User was not found.
-    * @apiUse UserNotFoundError
-    * */
-    app.get('/users/search', UsersController.search)
 }
